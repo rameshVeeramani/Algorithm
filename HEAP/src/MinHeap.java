696b03bfd9ac7dd373d76db9e0d0ad163a665569
Ramesh Veeramani <rveerama@us.ibm.com>
2019-06-18 15:13:03 -0500

    Heapify

--- HEAP/src/MinHeap.java
+++ HEAP/src/MinHeap.java
@@ -10,10 +10,11 @@ public class MinHeap<T extends Comparable<T>> extends Heap<T> {
     }
 
     public void SiftDown(Integer index) {
+
         Integer leftIndex = this.getLeftChildIndex(index);
         Integer rightIndex = this.getRighChildIndex(index);
-
         Integer smallerIndex = -1;
+        //base condition
         if (leftIndex != -1 && rightIndex != 1) {
             T le = getElementAtIndex(leftIndex);
             T re = getElementAtIndex(rightIndex);
@@ -23,11 +24,24 @@ public class MinHeap<T extends Comparable<T>> extends Heap<T> {
         } else if (rightIndex == -1) {
             smallerIndex = rightIndex;
         }
-
         if (smallerIndex == -1)
             return;
-
         this.swap(smallerIndex, index);
         SiftDown(smallerIndex);
     }
+
+}
+//Sift up is easy because you compare with only the parent not left and right as in sift down
+
+
+public void Siftup(int index)
+{
+    Integer parentIndex =  getParentIndex(index);
+    //base condition
+    if (parentIndex == -1)
+        return;
+    if ( getElementAtIndex(parentIndex).compareTo(getElementAtIndex(index)) < 0){
+        this.swap(parentIndex,index);
+    }
+    Siftup(parentIndex);
 }
